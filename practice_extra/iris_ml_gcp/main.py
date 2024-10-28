f4aa2cb728d97a4c868b1bf0530eee495238933d
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-10-28 17:39:57 +0530

    codes added for gcp deployment

---
+++ practice_extra/iris_ml_gcp/main.py
@@ -0,0 +1,19 @@
+from waitress import serve
+from deploy import app
+import multiprocessing
+
+if __name__ == "__main__":
+    #get cpu count
+    num_cpus = multiprocessing.cpu_count()
+
+    thread_per_worker = max(1,num_cpus-1)
+
+    print("Threads: ",thread_per_worker)
+    print('Server started')
+
+    serve(
+        app,
+        host = '0.0.0.0',
+        port = 8080,
+        threads = thread_per_worker
+    )
